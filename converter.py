f99b12451251c02171594079d8bf854f207b318c
mattu189 <yoshitika@icloud.com>
2020-03-08 22:08:56 +0900

    mp4が入力された場合mp3に変換する機能追加

--- converter.py
+++ converter.py
@@ -3,7 +3,7 @@ import os, sys, tkinter, tkinter.filedialog, tkinter.messagebox, ffmpeg, json
 root = tkinter.Tk()
 root.withdraw()
 
-fTyp = [("",".webm .mkv")]
+fTyp = [("",".webm .mkv .mp4")]
 iDir = os.path.abspath(os.path.dirname(__file__))
 file = tkinter.filedialog.askopenfilename(filetypes = fTyp,initialdir = iDir)
 filename = os.path.splitext(file)[0]
@@ -12,12 +12,6 @@ file_ext = os.path.splitext(file)[1][1:]
 if len(file) == 0:
     tkinter.messagebox.showerror('エラー', 'ファイルが選択されていません')
     sys.exit()
-
-file_check = filename + ".mp4"
-if os.path.exists(file_check) == True:
-    warning = tkinter.messagebox.askyesno('注意','すでに'+ filename +'.mp4ファイルが存在します。上書きしますか？')
-    if warning == False:
-        sys.exit()
 
 if file_ext == "mkv":
     (
@@ -33,6 +27,15 @@ elif file_ext == "webm":
     ffmpeg
     .input(file)
     .output(filename+".mp4")
+    .overwrite_output()
+    .run()
+    )
+
+elif file_ext == "mp4":
+    (
+    ffmpeg
+    .input(file)
+    .output(filename+".mp3", acodec="libmp3lame", ab="128k")
     .overwrite_output()
     .run()
     )
